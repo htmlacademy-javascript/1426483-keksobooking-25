7bb34ccfa6655b9d347876eebe890611761a9b27
Keks <keks@htmlacademy.ru>
2022-03-23 02:42:16 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/data.js
@@ -0,0 +1,65 @@
+const OFFER_COUNT = 10;
+
+const ROOMTOGUESTS = {
+  1: ['1'],
+  2: ['1', '2'],
+  3: ['1', '2', '3'],
+  100: ['0']
+};
+
+const TYPES = {
+  palace: 'Дворец',
+  flat: 'Квартира',
+  house: 'Дом',
+  bungalow: 'Бунгало',
+  hotel: 'Отель'
+};
+
+const TIMES = [
+  '12:00',
+  '13:00',
+  '14:00'
+];
+
+const FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner'
+];
+
+const DESCRIPTIONS = [
+  'under 5 min walk to Falls',
+  'Backyard Oasis-Pool',
+  'views of wine country',
+  'warm and comfy',
+  '2 bedroom plus hot tub',
+  '3 bedroom with free parking',
+  'short walk to the Falls',
+  'terrace, open kitchen with island',
+  'air conditioning, very bright',
+  'exceptional location perfect for spending the winter'
+];
+
+const TITLES = [
+  'Guest suite',
+  'Clean and stylish loft',
+  'Chic 3 bedroom home',
+  'Newly renovated cottage',
+  'Cheerful bungalow',
+  'Luxury stay on the River',
+  'Remodeled studio',
+  'The wanderlust room',
+  'Farmhouse loft',
+  'Modern apartment'
+];
+
+const PHOTOS_LINKS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
+];
+
+export {OFFER_COUNT, ROOMTOGUESTS, TYPES, TIMES, FEATURES, DESCRIPTIONS, TITLES, PHOTOS_LINKS};
